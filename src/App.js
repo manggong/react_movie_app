a021896cb692eefe31cb701d61b9899a607429d2
manggong <jihwank1995@gmail.com>
2019-12-10 09:10:15 +0900

    gitignore update

--- src/App.js
+++ src/App.js
@@ -11,9 +11,7 @@ class App extends React.Component {
   getMovies = async () => {
     const {
       data: {
-        data: {
-          movies
-        }
+        data: { movies }
       }
     } = await axios.get(
       "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
@@ -28,48 +26,31 @@ class App extends React.Component {
   }
 
   render() {
-    const {
-      isLoading,
-      movies
-    } = this.state;
-    return ( <
-      section className = "container" > {
-        isLoading ? ( <
-          div className = "loader" >
-          <
-          span className = "loader__text" > Loading... < /span> <
-          /div>
-        ) : ( <
-          div className = "movies" > {
-            movies.map(movie => ( <
-              Movie key = {
-                movie.id
-              }
-              id = {
-                movie.id
-              }
-              year = {
-                movie.year
-              }
-              title = {
-                movie.title
-              }
-              summary = {
-                movie.summary
-              }
-              poster = {
-                movie.medium_cover_image
-              }
-              genres = {
-                movie.genres
-              }
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {" "}
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text"> Loading... </span>{" "}
+          </div>
+        ) : (
+          <div className="movies">
+            {" "}
+            {movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
               />
-            ))
-          } <
-          /div>
-        )
-      } <
-      /section>
+            ))}{" "}
+          </div>
+        )}{" "}
+      </section>
     );
   }
 }
